5c8765b4e36a4cc7c34489512936c218ba908ab9
Ranjula001 <ranjulailukpitiya@gmail.com>
2026-05-21 18:56:52 +0530

    upstach

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from './config/config.js';
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
 import subscriptionRoutes from './routes/subscription.routes.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 import errorMiddleware from './middleware/error.middleware.js';
 import arcjetMiddleware from './middleware/arcjet.middleware.js';
@@ -21,6 +22,7 @@ app.use(arcjetMiddleware); //apply Arcjet middleware to all routes
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/subscriptions', subscriptionRoutes);
+app.use('/api/v1/workflows', workflowRouter);
 
 app.use(errorMiddleware);
 
